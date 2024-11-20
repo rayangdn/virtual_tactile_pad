135027c39fa4f43fea8b4695e81a560d375ed02f
rayangdn <rayan.gauderon@epfl.ch>
2024-11-20 11:25:33 +0100

    moved net-ft-ros to lib

---
+++ lib/net_ft_ros_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/rayann/Desktop/virtual_tactile_pad/lib/net_ft_ros_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/rayann/miniconda3/envs/ros_env'.split(';') if '/home/rayann/miniconda3/envs/ros_env' != '' else []
---
+++ lib/net_ft_ros_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/rayann/Desktop/virtual_tactile_pad/lib/net_ft_ros_ws/devel:$CMAKE_PREFIX_PATH"
+export PATH='/home/rayann/miniconda3/envs/ros_env/bin:/home/rayann/miniconda3/envs/ros_env/x86_64-conda-linux-gnu/sysroot/usr/bin:/home/rayann/miniconda3/envs/ros_env/share/rubygems/bin:/home/rayann/miniconda3/condabin:/opt/openrobots/bin:/opt/gurobi1103/linux64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin'
+export PKG_CONFIG_PATH='/home/rayann/miniconda3/envs/ros_env/lib/pkgconfig:/opt/openrobots/lib/pkgconfig'
+export PWD='/home/rayann/Desktop/virtual_tactile_pad/lib/net_ft_ros_ws/build'
+export PYTHONPATH="/home/rayann/miniconda3/envs/ros_env/lib/python3.11/site-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/rayann/Desktop/virtual_tactile_pad/lib/net_ft_ros_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/rayann/Desktop/virtual_tactile_pad/lib/net_ft_ros_ws/src:$ROS_PACKAGE_PATH"
